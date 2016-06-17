6475db8c25d3a78828ef2aaf7adaa13182a21afc
gaowenbo <948053013@qq.com>
2016-06-17 17:29:53 +0800

    更改一些设置

---
+++ config/local.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+
+  email: {
+   // default mail options
+	  mailOptions: {
+	  // by default log emails in console
+	  sendToConsole: false,
+	  // default from and to
+	  from: '948053013@qq.com', // sender address
+	  subject: 'Blog # ', // Subject line
+	  },
+   // connection configs
+   auth: {
+   user: '948053013@qq.com',
+    pass: 'tfkb@2012;'
+     },
+    // ignoreTLS: false,
+  // name: null,
+   // optional params
+   service: 'Mandrill',
+    type: 'SMTP',
+    host: 'smtp.qq.com',
+     port: 587,
+	 debug: true
+  //   // secure: 'true',
+  //   // localAddress: '',
+  //   // connectionTimeout: '',
+  //   // greetingTimeout: '',
+  //   // socketTimeout: '',
+
+  //   // authMethod: '',
+  //   // tls: ''
+  }
+
+
+}
